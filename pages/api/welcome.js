e7a54be837690c48b0142a34dc6464d7571204b2
Solomon Olatunji <realsolomon01@outlook.com>
2023-05-27 12:02:42 +0100

    fixing

--- pages/api/welcome.js
+++ pages/api/welcome.js
@@ -6,10 +6,10 @@ import { MongoClient } from 'mongodb';
 const handlebarOptions = {
   viewEngine: {
     extName: ".hbs",
-    partialsDir: path.resolve("./templates/"),
+    partialsDir: path.resolve("../../templates/"),
     defaultLayout: false,
   },
-  viewPath: path.resolve("./templates/"),
+  viewPath: path.resolve("../../templates/"),
   extName: ".hbs",
 };
 
@@ -18,20 +18,18 @@ async function ContactApi(req, res) {
 
   const transporter = nodemailer.createTransport({
     port: 587,
-    secure: false, // Set secure to false to use TLS instead of SSL
     host: process.env.CONTACT_FORM_HOST,
     auth: {
       user: process.env.CONTACT_FORM_SEND_EMAIL,
       pass: process.env.CONTACT_FORM_PASS,
     },
-    tls: { rejectUnauthorized: false }, // Disable SSL/TLS version check
   });
   transporter.use("compile", hbs(handlebarOptions));
 
   try {
     transporter.sendMail({
           from: `Spirenet`,
-          replyTo: email,
+          replyTo: process.env.CONTACT_FORM_RECEIVE_EMAIL,
           to: email,
           subject: `Thanks For Joining`,
           template: "welcome",
@@ -39,7 +37,6 @@ async function ContactApi(req, res) {
   
     // Save the email in MongoDB
     const uri = process.env.MONGODB_URI;
-    const MongoClient = require('mongodb').MongoClient;
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   
     await client.connect();
